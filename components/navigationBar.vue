765678835dd9844fc6278c2a00d272b7498196a3
Eðvald Atli Sigurvaldsson <edvaldatli@hotmail.com>
2024-04-27 18:26:45 +0000

    img maybe works

--- components/navigationBar.vue
+++ components/navigationBar.vue
@@ -5,12 +5,12 @@ import { useRoute } from 'vue-router'
 let route = useRoute().name;
 function computeLinkClass(targetRoute) {
     route = useRoute().name
-    return route === targetRoute ? 'text-blue-700' : null
+    return route === targetRoute ? 'text-gold' : null
 };
 
 </script>
 <template>
-    <div class="fixed bottom-0 w-full bg-slate-300 text-black h-14 py-4 z-10">
+    <div class="fixed bottom-0 w-full bg-dark text-gray h-14 py-4 z-10">
         <ul class="w-full flex flex-row justify-around text-center">
             <NuxtLink to="/" class="w-1/3" :class="computeLinkClass('index')">
                 <i class="pi pi-home" style="font-size: 1.5rem" />
