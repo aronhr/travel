1a988c5d6f55e916bbbf2163d45af094987a4db5
Eðvald Atli Sigurvaldsson <edvaldatli@hotmail.com>
2024-04-27 23:00:23 +0000

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="h-full flex flex-col justify-start text-white">
-    <img src="../static/landing-image.jpeg" class="-z-10 fixed w-screen h-screen blur-sm brightness-50 scale-110"/>
+    <img src="../public/landing-image.jpeg" class="-z-10 fixed w-screen h-screen blur-sm brightness-50 scale-110"/>
     <h1 class="text-6xl font-bold w-32 p-8"><p class="text-2xl text-nowrap">Velkomnir til</p> Edinborgar</h1>
     <div class="flex flex-col justify-center items-center h-2/3">
       <h3 class="text-xl w-1/2 text-center">Einhver texti Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque consequuntur officia eveniet officiis dolorum sunt voluptatibus distinctio ut, a inventore repudiandae ratione eaque facere ex ea quia reiciendis voluptatum numquam!</h3>
