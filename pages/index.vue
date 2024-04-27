f0abc1aacb18927206678de3246e45b9dd2bb6ed
Aron Hrafnsson <aronhr98@gmail.com>
2024-04-27 12:38:46 +0000

    directory structure

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script setup>
+// List of random names
+const names = ['John', 'Doe', 'Jane', 'Doe', 'Foo', 'Bar', 'Baz', 'Qux', 'Quux', 'Corge', 'Grault', 'Garply', 'Waldo', 'Fred', 'Plugh', 'Xyzzy', 'Thud'];
+
+// Randomly select a name
+const name = names[Math.floor(Math.random() * names.length)];
+
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">Hello {{ name }}</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
