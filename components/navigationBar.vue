9a5d656d266c138fadadd47d83ace29bd7410ab7
Eðvald Atli Sigurvaldsson <edvaldatli@hotmail.com>
2024-04-27 14:06:09 +0000

    Update navigationBar.vue

--- components/navigationBar.vue
+++ components/navigationBar.vue
@@ -4,8 +4,6 @@ import Divider from "primevue/divider"
 </script>
 <template>
     <div class="w-screen">
-
-<i class="pi pi-spin pi-spinner" style="font-size: 2rem"></i>
         <ul class="w-full flex flex-row justify-around text-center py-4">
             <li class="w-1/3">Home</li>
             <Divider layout="vertical" class="bg-black w-[2px]" />
