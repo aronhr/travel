344b79efc4e933151ddce690c758c80a5fb8e55f
Aron Hrafnsson <aronhr98@gmail.com>
2024-04-27 23:47:02 +0000

    geolocation and more places

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -83,7 +83,7 @@ export default defineNuxtConfig({
       installPrompt: true,
       // you don't need to include this: only for testing purposes
       // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
-      periodicSyncForUpdates: 20,
+      periodicSyncForUpdates: 3600,
     },
     devOptions: {
       enabled: true,
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,42 @@
+<script setup>
+
+// If user is located in Iceland set variable to false else set to true
+const isIceland = ref(false);
+const locationAllowed = ref(false);
+// Get user location
+const getLocation = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    console.log("Geolocation is not supported by this browser.");
+  }
+}
+
+// If location is in Iceland set variable to true
+const showPosition = (position) => {
+  if (position.coords.latitude > 63.5 && position.coords.latitude < 66.5 && position.coords.longitude > -24.5 && position.coords.longitude < -13.5) {
+    isIceland.value = true;
+  }
+}
+
+getLocation();
+
+
+
+</script>
+
 <template>
   <div>
     <div class="h-full flex flex-col justify-start text-white">
     <img src="../public/landing-image.jpeg" class="-z-10 fixed w-screen h-screen blur-sm brightness-50 scale-110"/>
-    <h1 class="text-6xl font-bold w-32 p-8"><p class="text-2xl text-nowrap">Velkomnir til</p> Edinborgar</h1>
+    <h1 class="text-6xl font-bold w-32 p-8"><p class="text-2xl text-nowrap">Velkomnir til </p> <span v-if="isIceland">Íslands</span> <span v-else>Edinborgar</span></h1>
     <div class="flex flex-col justify-center items-center h-2/3">
       <h3 class="text-xl w-1/2 text-center">Einhver texti Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque consequuntur officia eveniet officiis dolorum sunt voluptatibus distinctio ut, a inventore repudiandae ratione eaque facere ex ea quia reiciendis voluptatum numquam!</h3>
     </div>
   </div>
   </div>
-  
-  
+
+
 </template>
 
 <style scoped>
