357b461d32681c7fd940e78f3dfefbadcee0bf93
Eðvald Atli Sigurvaldsson <edvaldatli@hotmail.com>
2024-04-28 14:53:12 +0000

    dynamic routing from cards

--- components/Card.vue
+++ components/Card.vue
@@ -2,23 +2,24 @@
     <div class="flex flex-col p-2">
       <div class="bg-dark rounded shadow-md p-2">
         <!-- Card Image -->
-        <div v-if="imageUrl" class="p-3">
-            <img :src="imageUrl" class="rounded-t w-13 h-12">
+        <div v-if="place.imageUrl" class="p-3">
+            <img :src="place.imageUrl" class="rounded-t w-13 h-12">
         </div>
         <!-- Card Header -->
         <div class="border-b border-gray rounded-t">
-          <h2 class="text-lg font-semibold">{{ title }}</h2>
+          <h2 class="text-lg font-semibold">{{ place.title }}</h2>
         </div>
   
         <!-- Card Body -->
         <div class="p-3">
-          <p>{{ content }}</p>
+          <p>{{ place.description }}</p>
         </div>
   
         <!-- Card Footer -->
         <div class="border-t border-gray p-3 rounded-b">
           <div class="flex justify-end">
-            <button class="px-3 py-2 bg-dark text-white rounded-md"><i class='pi pi-map-marker'></i></button>
+            <NuxtLink :to="`/schedule/${place.id}`"><button class="px-3 py-2 bg-dark text-white rounded-md"><i class='pi pi-map-marker'></i></button></NuxtLink>
+            
           </div>
         </div>
       </div>
@@ -28,9 +29,7 @@
   <script>
   export default {
     props: {
-      imageUrl: String,
-      title: String,
-      content: String,
+      place: Object
     }
   };
   </script>
