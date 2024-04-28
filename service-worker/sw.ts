afb2c2618160a4d968096a07c559e01dc1ab4545
Aron Hrafnsson <aronhr98@gmail.com>
2024-04-28 10:39:24 +0000

    Update serviceworker for offline capabilities

--- service-worker/sw.ts
+++ service-worker/sw.ts
@@ -1,24 +1,45 @@
-import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
-import { clientsClaim } from 'workbox-core'
-import { NavigationRoute, registerRoute } from 'workbox-routing'
+import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching'
+import { clientsClaim, setCacheNameDetails } from 'workbox-core'
+import { registerRoute, NavigationRoute } from 'workbox-routing'
+import { CacheFirst } from 'workbox-strategies'
+import { ExpirationPlugin } from 'workbox-expiration'
 
 declare let self: ServiceWorkerGlobalScope
 
-// self.__WB_MANIFEST is default injection point
-precacheAndRoute(self.__WB_MANIFEST)
+// Setting up cache names
+setCacheNameDetails({
+    prefix: 'my-app',
+    suffix: 'v1',
+    precache: 'precache',
+    runtime: 'runtime',
+});
 
-// clean old assets
-cleanupOutdatedCaches()
+// Manually add items to precache
+precacheAndRoute([
+    ...self.__WB_MANIFEST,
+    { url: '../content/places.json', revision: '12345' } // Add the path to your JSON file and a revision for cache busting
+]);
 
-let allowlist: undefined | RegExp[]
-if (import.meta.env.DEV)
-    allowlist = [/^\/$/]
+// Navigation Route for HTML (Single Page App)
+const navigationHandler = createHandlerBoundToURL('/')
+registerRoute(new NavigationRoute(navigationHandler, {
+    allowlist: [new RegExp('^/')],
+}))
 
-// to allow work offline
-registerRoute(new NavigationRoute(
-    createHandlerBoundToURL('/'),
-    { allowlist },
-))
+// Cache strategy for JSON files
+registerRoute(
+    ({ url }) => url.pathname.endsWith('.json'),
+    new CacheFirst({
+        cacheName: 'json-cache',
+        plugins: [
+            new ExpirationPlugin({
+                maxEntries: 10, // Adjust depending on how many JSON files you have
+                maxAgeSeconds: 24 * 60 * 60, // 1 day
+            }),
+        ],
+    }),
+);
 
+// Claim clients and skip waiting
 self.skipWaiting()
 clientsClaim()
