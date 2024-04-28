7e7a6f6d5edf423c790ecd0f51cbb937f1f5a0a1
Aron Hrafnsson <aronhr98@gmail.com>
2024-04-28 17:55:05 +0000

    fix btn

--- pages/schedule/[id].vue
+++ pages/schedule/[id].vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<script setup>
 // get slug from url
 import usePlaces from "~/composables/usePlaces.js";
 
@@ -24,21 +24,40 @@ const getTime = (timestamp) => {
   return new Date(timestamp).toLocaleString('is-IS', { hour: 'numeric', minute: 'numeric', hour12: false });
 }
 
-const openMaps = () => {
+const link = ref('');
+
+onMounted(() => {
+  const isAppleDevice = () => {
+    return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+  };
+
+  // Check if geolocation is supported
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
-      if /* if we're on iOS, open in Apple Maps */
-      (isApple){
-        window.open(`https://maps.apple.com/maps?saddr==${position.coords.latitude},${position.coords.longitude}&daddr=${place?.coordinates.lat},${place?.coordinates.lng}`)
-      }
-      else /* else use Google */
-        window.open(`https://www.google.com/maps/dir/?api=1&origin=${position.coords.latitude},${position.coords.longitude}&destination=${place?.coordinates.lat},${place?.coordinates.lng}&travelmode=walking`);
+      const { latitude, longitude } = position.coords;
+      const destinationLat = place?.coordinates?.lat;
+      const destinationLng = place?.coordinates?.lng;
 
+      // Ensure place coordinates are defined
+      if (destinationLat == null || destinationLng == null) {
+        console.error("Destination coordinates are not defined.");
+        return;
+      }
+
+      // Open the relevant maps application based on the device
+      if (isAppleDevice()) {
+        link.value = `https://maps.apple.com/maps?saddr=${latitude},${longitude}&daddr=${destinationLat},${destinationLng}`
+      } else {
+        link.value = `https://www.google.com/maps/dir/?api=1&origin=${latitude},${longitude}&destination=${destinationLat},${destinationLng}&travelmode=walking`
+      }
+    }, () => {
+      console.error("Failed to retrieve your position.");
     });
   } else {
     console.log("Geolocation is not supported by this browser.");
   }
-}
+})
+
 
 </script>
 
@@ -54,7 +73,7 @@ const openMaps = () => {
     </div>
     <div class="flex flex-col justify-start items-start w-full">
       <p>{{ place.description }}</p>
-      <button class="bg-gold w-full" @click="openMaps()">{{ place?.location }}</button>
+      <nuxt-link :to="link" class="bg-gold w-full text-center">{{ place?.location }}</nuxt-link>
     </div>
   </div>
 </template>
