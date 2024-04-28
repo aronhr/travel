53e4aa4e9ec5dd912cc2d18864ea72655ca3fbb9
ZhivkoDimov <zhivkod@vodafone.is>
2024-04-28 21:41:48 +0000

    breyta cards og texta a home page

--- components/Card.vue
+++ components/Card.vue
@@ -2,17 +2,17 @@
   <NuxtLink :to="`/schedule/${place.id}`">
     <div class="p-2">
       <div class="  bg-dark justify-between min-h-96 rounded shadow-md flex flex-col p-2 bg-cover bg-center" :style="{ 'background-image': 'url('+ place.imageUrl +')' }">
-      	<div class="w-fit bg-dark p-3 border-b border-gray rounded-lg">
+      	<div class="w-fit bg-dark bg-opacity-70 p-3 rounded-lg">
             <h2 class="text-lg font-semibold">{{ place.title }}</h2>
         </div>
         <!-- Card Header -->
-        <div class="bg-dark p-2 rounded-lg  ">  
+        <div class="bg-dark bg-opacity-70 p-2 rounded-lg  ">  
           <!-- Card Body -->
           <div class="p-3">
             <p>{{ place.description }}</p>
           </div>
           <!-- Card Footer -->
-          <div class="border-t border-gray p-3 rounded-b flex flex-row justify-between">
+          <div class="p-3 rounded-b flex flex-row justify-between">
             <p class="px-3 py-2">{{ place.time }}</p>
             <p class="px-3 py-2">{{ place.location }}</p> 
           </div>
