b2dd4d3a77537f916afaa1716f95d92b5c9521bc
Eðvald Atli Sigurvaldsson <edvaldatli@hotmail.com>
2024-05-02 22:01:08 +0000

    booking time on cards hidden and more

--- components/Card.vue
+++ components/Card.vue
@@ -11,7 +11,6 @@
         <div class="w-fit bg-dark z-10 bg-opacity-70 max-w-60 p-3 rounded-lg col-span-2 justify-self-start row-start-1 flex items-center">
             <h2 class="text-lg z-10 font-semibold truncate">{{ place.title }}</h2>
         </div>
-
         <!-- Card Header -->
         <div class="bg-dark z-10 bg-opacity-70 p-2 rounded-lg row-start-4 col-span-3 justify-self-stretch">
           <!-- Card Body -->
@@ -19,16 +18,17 @@
             <p class="line-clamp-3 italic font-medium">{{ place.description }}</p>
           </div>
           <!-- Card Footer -->
-          <div class="grid grid-cols-3 p-3 rounded-b font-bold">
-            <div class="col-span-1 flex flex-row items-center gap-2">
-                <i class="pi pi-clock"></i>
+          <div class="grid grid-cols-3 p-3 font-bold">
+            <div class="col-span-1 flex flex-row items-center gap-2" v-if="place.time">
+              <i class="pi pi-clock"></i>
               <div class="flex flex-col">
-              <p class="">{{ getTime(place.time) }}</p>
-              <p class="">{{ getDate(place.time) }}</p>
+                <p class="">{{ getTime(place.time) }}</p>
+                <p class="">{{ getDate(place.time) }}</p>
+              </div>
             </div>
-            </div>
-            <div class="px-1 py-1 col-start-2 col-span-2 flex flex-row items-center gap-2">
-              <i class="pi pi-map-marker"></i>{{ place.address }}
+            <div :class="`px-1 py-1 flex flex-row items-center gap-2 ${place.time ? 'col-start-2 col-span-2' : 'col-span-3'}`">
+              <i class="pi pi-map-marker" />
+              <p class="line-clamp-2">{{ place.address }}</p>
             </div>
           </div>
         </div>
