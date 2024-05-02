eb900d1988aebedaa9e49cc0e68740a9978c8a03
Aron Hrafnsson <aronhr98@gmail.com>
2024-05-02 21:22:55 +0000

    fix favorite again

--- components/Card.vue
+++ components/Card.vue
@@ -3,14 +3,17 @@
     <div class="p-2 relative">
       <div class="grid grid-cols-3 grid-rows-3 gap-1 min-h-96 rounded-xl shadow-md p-2 z-10">
       	<div class="bg-dark z-10 bg-opacity-70 p-2 rounded-lg col-start-3 row-start-1 w-10 justify-self-end flex items-center justify-center flex-row">
-          <h2 class="text-lg font-semibold"><i class="pi pi-star"></i></h2>
+          <h2 class="text-lg font-semibold" @click="saveFavorite(place.id); isFav = !isFav">
+            <i v-if="isFav" class="pi pi-star-fill text-yellow-400"></i>
+            <i v-else class="pi pi-star"></i>
+          </h2>
         </div>
         <div class="w-fit bg-dark z-10 bg-opacity-70 max-w-60 p-3 rounded-lg col-span-2 justify-self-start row-start-1 flex items-center">
             <h2 class="text-lg z-10 font-semibold truncate">{{ place.title }}</h2>
         </div>
-        
+
         <!-- Card Header -->
-        <div class="bg-dark z-10 bg-opacity-70 p-2 rounded-lg row-start-4 col-span-3 justify-self-stretch">  
+        <div class="bg-dark z-10 bg-opacity-70 p-2 rounded-lg row-start-4 col-span-3 justify-self-stretch">
           <!-- Card Body -->
           <div class="p-3 col-span-3">
             <p class="line-clamp-3 italic font-medium">{{ place.description }}</p>
@@ -35,10 +38,17 @@
   </NuxtLink>
 </template>
 
-  <script setup>
-    import useDate from "~/composables/useDate.js";
-    const { getTime, getDate } = useDate();
-    const props = defineProps ({
-    place: Object
-    })
-  </script>
+<script setup>
+import useDate from "~/composables/useDate.js";
+import useFavorite from "~/composables/useFavorite.js";
+const { getTime, getDate } = useDate();
+const props = defineProps ({
+  place: Object
+})
+
+const { isFavorite, saveFavorite } = useFavorite();
+
+const isFav = ref(false);
+isFav.value = isFavorite(props.place.id);
+
+</script>
